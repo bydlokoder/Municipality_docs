15213cc36d75eda4452f140686dd20229845a17f
Андрей <kudrandrew@gmail.com>
2015-06-08 19:36:49 +0300

    AnswerDBClassImpl refactoring

--- src/com/company/persistance/AnswerDBClassImpl.java
+++ src/com/company/persistance/AnswerDBClassImpl.java
@@ -6,10 +6,12 @@ import com.company.persistance.interfaces.AnswerDBClassI;
 import java.sql.*;
 
 public class AnswerDBClassImpl implements AnswerDBClassI {
+    public static final String ANSWERS_TABLE = "answers";
+
     @Override
     public Answer getAnswer(int id) throws SQLException {
         Answer result = null;
-        String sql = "SELECT * FROM answers WHERE id=?";
+        String sql = String.format("SELECT * FROM %s WHERE %s=?", ANSWERS_TABLE, Answer.COLUMN_ID);
         Connection connection = DBManager.getConnection();
         PreparedStatement statement = connection.prepareStatement(sql);
         statement.setInt(1, id);
@@ -26,9 +28,7 @@ public class AnswerDBClassImpl implements AnswerDBClassI {
 
     @Override
     public int createAnswer(Answer answer) throws SQLException {
-        String insertAnswerSQL = "INSERT INTO answers" + "(" + Answer.COLUMN_QUERY_ID + ","
-                + Answer.COLUMN_REG_DATE + "," + Answer.COLUMN_ANSWER + ")" + " VALUES "
-                + "(?,?,?)";
+        String insertAnswerSQL = String.format("INSERT INTO %s (%s,%s,%s) VALUES (?,?,?)", ANSWERS_TABLE, Answer.COLUMN_QUERY_ID, Answer.COLUMN_REG_DATE, Answer.COLUMN_ANSWER);
         Connection connection = DBManager.getConnection();
         PreparedStatement statement = connection.prepareStatement(insertAnswerSQL);
         statement.setInt(1, answer.getQueryId());
@@ -39,7 +39,7 @@ public class AnswerDBClassImpl implements AnswerDBClassI {
 
     @Override
     public int deleteAnswer(int id) throws SQLException {
-        String delAnswerSQL = "DELETE FROM answers WHERE id=?";
+        String delAnswerSQL = String.format("DELETE FROM %s WHERE %s=?", ANSWERS_TABLE, Answer.COLUMN_ID);
         Connection connection = DBManager.getConnection();
         PreparedStatement statement = connection.prepareStatement(delAnswerSQL);
         statement.setInt(1, id);
@@ -48,8 +48,7 @@ public class AnswerDBClassImpl implements AnswerDBClassI {
 
     @Override
     public int updateAnswer(Answer answer) throws SQLException {
-        String updateAnswerSQL = "UPDATE answers SET " + Answer.COLUMN_QUERY_ID + "=?, " + Answer.COLUMN_ANSWER + "=?, "
-                + Answer.COLUMN_REG_DATE + "=? WHERE id=?";
+        String updateAnswerSQL = String.format("UPDATE %s SET %s=?, %s=?, %s=? WHERE %s=?", ANSWERS_TABLE, Answer.COLUMN_QUERY_ID, Answer.COLUMN_ANSWER, Answer.COLUMN_REG_DATE, Answer.COLUMN_ID);
         Connection connection = DBManager.getConnection();
         PreparedStatement statement = connection.prepareStatement(updateAnswerSQL);
         statement.setInt(1, answer.getQueryId());
