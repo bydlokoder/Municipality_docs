ed310f36010ae6b849ba91840791cea5f26aaf4d
Андрей <kudrandrew@gmail.com>
2015-06-09 22:50:33 +0300

    Removed unused setters

--- src/com/company/entities/Department.java
+++ src/com/company/entities/Department.java
@@ -16,15 +16,7 @@ public class Department {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
